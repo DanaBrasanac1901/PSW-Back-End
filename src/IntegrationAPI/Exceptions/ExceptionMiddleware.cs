6d38cf526ad67eee3a9bfaa111b6941f76a949c0
petkovic82 <petkovic.ra82.2019@uns.ac.rs>
2022-11-02 22:19:39 +0100

    changed error handler

--- src/IntegrationAPI/Exceptions/ExceptionMiddleware.cs
+++ src/IntegrationAPI/Exceptions/ExceptionMiddleware.cs
@@ -1,31 +1,36 @@
 using System;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace IntegrationAPI.Exceptions
 {
-    public class ExceptionMiddleware{
-
-        private readonly RequestDelegate next;
-        public ExceptionMiddleware(RequestDelegate next) { this.next = next;}
-
-        public async Task InvokeAsync(HttpContext httpContext){
-            
-            try {await next(httpContext);}
-            catch(Exception ex){
-                httpContext.Response.StatusCode = (int)ExceptionStatusCodes.GetExceptionStatusCode(ex);
-                await httpContext.Response.WriteAsync(ex.Message);}
-
-
-        }
-    }
-    public static class MiddlewareExtensions
+    public class ExceptionMiddleware: IMiddleware
     {
-        public static void UseExceptionMiddleware(this IApplicationBuilder builder)
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            builder.UseMiddleware<ExceptionMiddleware>();
-        }
-    }
+            try
+            {
+                await next(context);
+            }
+            catch (BloodBankArgumentException error)
+            {
+                var response = context.Response;
+                //Set response ContentType
+                response.ContentType = "application/json";
+                response.StatusCode = (int)HttpStatusCode.BadRequest;
+                var responseContent = new ResponseContent()
+                {
+                    Error = error.Message
+                };
+                //Using Newtonsoft.Json to convert object to json string
+                var jsonResult = JsonConvert.SerializeObject(responseContent);
+                await context.Response.WriteAsync(jsonResult);
+            }
 
+        }
+
+    }
 }
