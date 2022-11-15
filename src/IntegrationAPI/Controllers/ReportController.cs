ec06c085ed796b541167be19608e63d155a0d470
petkovic82 <petkovic.ra82.2019@uns.ac.rs>
2022-11-15 20:59:52 +0100

    generating report

--- src/IntegrationAPI/Controllers/ReportController.cs
+++ src/IntegrationAPI/Controllers/ReportController.cs
@@ -1,18 +1,84 @@
+using System;
 using IntegrationLibrary.Report;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IntegrationAPI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    
     public class ReportController : Controller
     {
         private readonly IReportService _reportService;
-        public ReportController(IReportService reportService)
-        {
-            _reportService = reportService;
-        }
+        private readonly ReportGenerator _reportGenerator;
+            public ReportController(IReportService reportService)
+            {
+                _reportService = reportService;
+            }
 
-        public void Generate()
-        {
-        }
+            // GET: api/reports ili bez s?
+            [HttpGet]
+            public ActionResult GetAll()
+            {
+                return Ok(_reportService.GetAll());
+            }
+
+            // GET api/reports/2
+            [HttpGet("{id}")]
+            public ActionResult GetById(Guid id)
+            {
+                var report = _reportService.GetById(id);
+                if (report == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(report);
+            }
+
+            // POST api/reports
+            [HttpPost]
+            public ActionResult Create(Report report)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                _reportService.Create(report);
+                return CreatedAtAction("GetById", new { id = report.Id }, report);
+            }
+
+            // PUT api/reports/2
+            [HttpPut("{id}")]
+            public ActionResult Update(Guid id, Report report)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if (id != report.Id)
+                {
+                    return BadRequest();
+                }
+
+                try
+                {
+                    _reportService.Update(report);
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+
+                return Ok(report);
+            }
+
+
+            public void GeneratePDF(Guid id)
+            {
+                _reportGenerator.GeneratePDF(id);
+            }
     }
-}
+    }
--- src/IntegrationLibrary/Report/Report.cs
+++ src/IntegrationLibrary/Report/Report.cs
@@ -1,23 +1,27 @@
 using System;
+using System.Collections.Generic;
+using HospitalLibrary.Core.Blood;
+using Nest;
 
 namespace IntegrationLibrary.Report
 {
     public class Report
     {
         public Guid Id { get; set; }
-
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-
-        public DateTime ConfigurationDate { get; set; } //u create staviti Today
-        public int GeneratingPeriod { get; set; } //na koliko dana da se generise 
-        public int BloodBankId { get; set; } 
+        public Guid BloodbankId { get; set; }
+        public DateTime ConfigurationDate { get; set; }
+        public Period Period { get; set; } 
         
+      //  public int ConsumptionAmount { get; set; }  ubaci kad se bude generisao pdf
+       
+        
+        public Report(Guid id, Guid bloodbankId, DateTime configurationDate, Period period)
+        {
+            Id = id;
+            BloodbankId = bloodbankId;
+            ConfigurationDate = configurationDate;
+            Period = period;
+        }
         
     }
-    
-    
-    //metoda koja enum pretvara u period ili mzd ima neka njihova biblioteka nez 
-    //metoda koja sabira consumption, mzd polje da se doda? ili metoda koja na osnovu pocetnog i krajnjeg datuma
-    //i na osnovu banke id dobavlja za svaki dan od te banke potrosnju i sabere 
 }
