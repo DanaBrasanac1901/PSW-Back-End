12e0b11cf78b1e0d27ffde15a0679e30a2237c63
Petar Obradovic <obradovicp3@gmail.com>
2022-11-18 19:26:17 +0100

    Added Diagnosis field in InpatientTreatmentRecord class

--- src/HospitalLibrary/Core/InpatientTreatmentRecord/InpatientTreatmentRecord.cs
+++ src/HospitalLibrary/Core/InpatientTreatmentRecord/InpatientTreatmentRecord.cs
@@ -13,6 +13,7 @@ namespace HospitalLibrary.Core.InpatientTreatmentRecord
         public string PatientID { get; set; }
         public string RoomID { get; set; }
         public string BedID { get; set; }
+        public string Diagnosis { get; set; }
         public DateTime AdmissionTime { get; set; }
         public Boolean Status { get; set; }
         public string Therapy { get; set; }
