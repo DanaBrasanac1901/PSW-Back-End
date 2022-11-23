e81a342f781eb728ebd51d21f3a180567ff175ef
petkovic82 <petkovic.ra82.2019@uns.ac.rs>
2022-11-23 12:45:17 +0100

    guid added

--- src/IntegrationLibrary/Settings/IntegrationDbContext.cs
+++ src/IntegrationLibrary/Settings/IntegrationDbContext.cs
@@ -19,9 +19,9 @@ namespace IntegrationLibrary.Settings
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            BloodBank.BloodBank bank1 = new BloodBank.BloodBank() { Id = Guid.NewGuid(), Username = "101A", Password = "edhb", Apikey = "efwfe", Email = "andykesic123@gmail.com", IsConfirmed = true };
-            BloodBank.BloodBank bank2 = new BloodBank.BloodBank() { Id = Guid.NewGuid(), Username = "101A", Password = "fewsfd", Apikey = "dqad", Email = "andykesic123@gmail.com", IsConfirmed = true };
-            BloodBank.BloodBank bank3 = new BloodBank.BloodBank() { Id = Guid.NewGuid(), Username = "101A", Password = "fcsde", Apikey = "ads", Email = "andykesic123@gmail.com", IsConfirmed = true };
+            BloodBank.BloodBank bank1 = new BloodBank.BloodBank() { Id = new Guid("2D4894B6-02E4-4288-A3D3-089489563190"), Username = "101A", Password = "edhb", Apikey = "efwfe", Email = "andykesic123@gmail.com", IsConfirmed = true };
+            BloodBank.BloodBank bank2 = new BloodBank.BloodBank() { Id = new Guid("55510651-D36E-444D-95FB-871E0902CD7E"), Username = "101A", Password = "fewsfd", Apikey = "dqad", Email = "andykesic123@gmail.com", IsConfirmed = true };
+            BloodBank.BloodBank bank3 = new BloodBank.BloodBank() { Id = new Guid("A60460FE-0D33-478D-93B3-45D424079E66"), Username = "101A", Password = "fcsde", Apikey = "ads", Email = "andykesic123@gmail.com", IsConfirmed = true };
 
            Report.Report report1 = new Report.Report( 
                                                bank3.Id,
