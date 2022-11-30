f6aa7a191b5a2ad9ebc3bdde5f661ca5ee922a68
petkovic82 <petkovic.ra82.2019@uns.ac.rs>
2022-11-30 19:50:22 +0100

    sending service

--- src/IntegrationLibrary/SendingData/SendingService.cs
+++ src/IntegrationLibrary/SendingData/SendingService.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Http;
 using IronPdf;
 using Microsoft.AspNetCore.Components.RenderTree;
 namespace IntegrationLibrary.Report.SendingReport
 
 {
-    public class SendingReportService : ISendingReportService
+    public class SendingService : ISendingService
     {
-        public IHttpActionResult SendReport(PdfAttachment pdf, Guid id)
+     /*   public IHttpActionResult SendReport(PdfAttachment pdf, Guid id)
         {
             throw new NotImplementedException();
         }
+
+        public IHttpActionResult SendUrgentBloodSupplyRequest(UrgentBloodRequest urgentBloodRequest)
+        {
+            throw new NotImplementedException();
+        } */
     }
 }
