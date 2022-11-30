4eda56224c858b3f181b459130ab806d3bc8dc09
FStefanovv <filipficastefanov22@gmail.com>
2022-11-30 21:34:21 +0100

    value object created

--- src/HospitalLibrary/Core/Blood/BloodSupply.cs
+++ src/HospitalLibrary/Core/Blood/BloodSupply.cs
@@ -1,47 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using HospitalLibrary.Core.Enums; 
 
 namespace HospitalLibrary.Core.Blood
 {
-    public class BloodSupply
+    public class BloodSupply : ValueObject
     {
-        
-        public int Id { get; set; }
         public BloodType Type { get; set; }
         public double Amount { get; set; }
         public Guid SourceBank { get; set; }
 
-
         public BloodSupply() { }
         
-        public BloodSupply(int id, BloodType type, double amount, Guid sourceBank)
-        {
-            this.Id = id;
-            this.Type = type;
-            this.Amount = amount;
-            this.SourceBank = sourceBank;
-        }
-
         public BloodSupply(BloodType type, double amount, Guid sourceBank)
         {
+            Validate(amount);
             this.Type = type;
             this.Amount = amount;
             this.SourceBank = sourceBank;
         }
 
-        public bool ReduceBy(double amount)
+        private void Validate(double amount)
         {
-            if (this.Amount >= amount)
-            {
-                this.Amount -= amount;
-                return true;
-            }
+            if (amount < 0)
+                throw new ArgumentException();
+        }
 
-            return false;
+        public BloodSupply ReduceBy(BloodSupply supply)
+        {
+            
+            bool canBeReduced = IsCompatible(supply) && IsReducibleBy(supply);
+            if (!canBeReduced) throw new ArgumentException();
+            {
+                return new BloodSupply(Type, Amount - supply.Amount, SourceBank);
+            }
+        }
+
+        private bool IsReducibleBy(BloodSupply supply)
+        {
+            return (Amount - supply.Amount) >= 0;
+        }
+
+        private bool IsCompatible(BloodSupply supply)
+        {
+            return supply.SourceBank == SourceBank && supply.Type == Type;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Type;
+            yield return Amount;
+            yield return SourceBank;
         }
     }
 }
