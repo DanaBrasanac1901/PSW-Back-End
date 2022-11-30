6a85d78a8c0ba909760eb8e7cefb866ab1cd31bb
slavko024 <icex024@gmail.com>
2022-11-30 16:49:27 +0100

    work on controller

--- src/HospitalAPI/Controllers/ReportController.cs
+++ src/HospitalAPI/Controllers/ReportController.cs
@@ -1,6 +1,35 @@
-﻿namespace HospitalAPI.Controllers
+﻿using HospitalLibrary.Core.Report.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalAPI.Controllers
 {
-    public class ReportController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
     {
+        private readonly IDrugApplicationService _drugApplicationService;
+        private readonly ISymptomApplicationService _symptomApplicationService;
+
+        public ReportController(IDrugApplicationService drugApplicationService, ISymptomApplicationService symptomApplicationService)
+        {
+            _drugApplicationService = drugApplicationService;
+            _symptomApplicationService = symptomApplicationService;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult GetAllDrugs()
+        {
+            return Ok(_drugApplicationService.GetAll());
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult GetAllSymptoms()
+        {
+            return Ok(_symptomApplicationService.GetAllSymptoms());
+        }
+
+
     }
 }
