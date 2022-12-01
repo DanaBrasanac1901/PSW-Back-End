2232255139bb4a1a297faab3fab7a2d7ff654f59
tibbers-7 <dmitrovic.ra41.2019@uns.ac.rs>
2022-12-01 19:00:15 +0100

    test fixes

--- HospitalAPITestProject/Integration/RegistrationTest.cs
+++ HospitalAPITestProject/Integration/RegistrationTest.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System;
 using HospitalLibrary.Core.User;
+using HospitalLibrary.Core.EmailSender;
 
 namespace HospitalTests.Integration
 {
@@ -18,7 +19,7 @@ namespace HospitalTests.Integration
 
         private static PatientsController SetupController(IServiceScope scope)
         {
-            return new PatientsController(scope.ServiceProvider.GetRequiredService<IPatientService>(),scope.ServiceProvider.GetRequiredService<IUserService>());
+            return new PatientsController(scope.ServiceProvider.GetRequiredService<IPatientService>(),scope.ServiceProvider.GetRequiredService<IUserService>(), scope.ServiceProvider.GetRequiredService<IEmailSendService>());
 
         }
         [Fact]
