66368116a1f3985580e32ba71e5878c2a615d589
Andrej132 <ruzic.ra104.2019@uns.ac.rs>
2022-12-12 18:36:08 +0100

    done

--- src/HospitalLibrary/Core/DateTimeRange.cs
+++ src/HospitalLibrary/Core/DateTimeRange.cs
@@ -1,21 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HospitalLibrary.Core
 {
-    public class DateTimeRange
+    public class DateTimeRange : ValueObject
     {
         public DateTimeRange(DateTime start, DateTime end)
         {
+            Validate(start,end);
             Start = start;
             End = end;
         }
 
-        public DateTime Start { get; set; }
-        public DateTime End { get; set; }
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
 
         public bool OverlapsWith(DateTimeRange range)
         {
@@ -25,6 +29,23 @@ namespace HospitalLibrary.Core
                 return true;
             else
                 return false;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Start;
+            yield return End;
+        }
+
+        public bool Include(DateTime date)
+        {
+            return date >= Start && date <= End;
+        }
+        
+        private void Validate(DateTime start, DateTime end)
+        {
+            if (end < start)
+                throw new ArgumentOutOfRangeException();
         }
     }
 }
