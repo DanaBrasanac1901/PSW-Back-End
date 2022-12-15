0d88bfdbad4a0975204c23cc16905ab2a08ec50b
Andrej132 <ruzic.ra104.2019@uns.ac.rs>
2022-12-15 20:10:32 +0100

    daterange-VO done

--- src/HospitalLibrary/Core/DateTimeRange.cs
+++ src/HospitalLibrary/Core/DateTimeRange.cs
@@ -7,16 +7,17 @@ using System.Threading.Tasks;
 
 namespace HospitalLibrary.Core
 {
-    public class DateTimeRange
+    public class DateTimeRange : ValueObject
     {
         public DateTimeRange(DateTime start, DateTime end)
         {
+            Validate(start, end);
             Start = start;
             End = end;
         }
 
-        public DateTime Start { get; set; }
-        public DateTime End { get; set; }
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
 
         public bool OverlapsWith(DateTimeRange range)
         {
@@ -27,6 +28,24 @@ namespace HospitalLibrary.Core
             else
                 return false;
         }
+
+        public bool Include(DateTime date)
+        {
+            return date >= Start && date <= End;
+        }
+
+        private void Validate(DateTime start, DateTime end)
+        {
+            if (end < start)
+                throw new ArgumentOutOfRangeException();
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Start;
+            yield return End;
+        }
+
 
         public string GetStartString()
         {
