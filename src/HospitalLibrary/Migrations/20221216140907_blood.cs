23e83699d8ac3775c77a8679b3b6106497ad29f9
Milos Adnadjevic <adnadjevic.ra77.2019@uns.ac.rs>
2022-12-16 17:37:45 +0100

    dobro biti nece kad te nadjem

---
+++ src/HospitalLibrary/Migrations/20221216140907_blood.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalLibrary.Migrations
+{
+    public partial class blood : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "HospitalBlood",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.InsertData(
+                table: "HospitalBlood",
+                columns: new[] { "Id", "Amount", "SourceBank", "Type" },
+                values: new object[] { 10, 58.0, new Guid("2d4894b6-02e4-4288-a3d3-089489563190"), 0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "HospitalBlood",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.InsertData(
+                table: "HospitalBlood",
+                columns: new[] { "Id", "Amount", "SourceBank", "Type" },
+                values: new object[] { 1, 54.0, new Guid("2d4894b6-02e4-4288-a3d3-089489563190"), 0 });
+        }
+    }
+}
