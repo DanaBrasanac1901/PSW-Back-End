f9ec948afcc7a648176244cd13d9a3ae61690db1
DanaBrasanac <brasanac.ra239.2018@uns.ac.rs>
2023-01-10 00:48:22 +0100

    pokusaj sredjivanja developa

---
+++ src/HospitalLibrary/Core/ValueObject1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Core
+{
+    public abstract class ValueObject1<T> where T : ValueObject1<T>
+    {
+        public override bool Equals(object obj)
+        {
+            var valueObject = obj as T;
+
+            if (ReferenceEquals(valueObject, null))
+                return false;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            return EqualsCore(valueObject);
+        }
+
+        protected abstract bool EqualsCore(T other);
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeCore();
+        }
+
+        protected abstract int GetHashCodeCore();
+
+        public static bool operator ==(ValueObject1<T> a, ValueObject1<T> b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueObject1<T> a, ValueObject1<T> b)
+        {
+            return !(a == b);
+        }
+    }
+}
